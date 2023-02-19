445c124ec87fa656ab13c52a4eb569f35d2f5b84
ilotterytea <ilotterytea@proton.me>
2023-02-20 01:10:44 +0600

    load holidays

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate lazy_static;
+
+mod holiday;
+
+use std::fs::{self, File};
+
+use holiday::Holiday;
 
 #[get("/")]
 fn index() -> &'static str {
     "yo!"
+}
+
+lazy_static! {
+    static ref HOLIDAYS: Vec<Vec<Holiday>> = {
+        let mut vec: Vec<Vec<Holiday>> = Vec::new();
+
+        let paths = fs::read_dir("./data").unwrap();
+
+        for path in paths {
+            let file = File::open(path.unwrap().path()).unwrap();
+
+            let _vec: Vec<Holiday> =
+                serde_json::from_reader(file).expect("JSON was not well-formatted!");
+
+            vec.push(_vec);
+        }
+
+        vec
+    };
 }
 
 #[launch]
