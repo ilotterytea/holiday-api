28dab728362ac62b6df2b110ab725db084f72327
ilotterytea <me@ilotterytea.kz>
2025-03-27 02:18:42 +0500

    feat: timezone support

--- public/index.php
+++ public/index.php
@@ -1,8 +1,17 @@
 <?php
 include_once "../holidays.php";
 
-$month = $_GET["month"] ?? date("n");
-$day = $_GET["day"] ?? date("d");
+$time = time();
+$utc = intval($_GET["utc"] ?? "");
+
+if (-12 <= $utc && $utc <= 12) {
+    $time += 60 * 60 * $utc;
+} else {
+    $utc = 0;
+}
+
+$month = $_GET["month"] ?? date("n", $time);
+$day = $_GET["day"] ?? date("d", $time);
 
 $holidays = [];
 
@@ -24,11 +33,15 @@ $holiday_count = count($holidays);
 <html>
 
 <head>
-    <title><?php echo $holiday_count ?> holidays on <?php echo "$day.$month" ?></title>
+    <title><?php echo $holiday_count ?> holidays on <?php echo "$day.$month" ?>
+        <?php echo $utc != 0 ? ("(UTC" . ($utc >= 0 ? "+" : "") . "$utc)") : "" ?>
+    </title>
 </head>
 
 <body>
-    <h1><?php echo $holiday_count ?> holidays on <?php echo "$day.$month" ?></h1>
+    <h1><?php echo $holiday_count ?> holidays on <?php echo "$day.$month" ?>
+        <?php echo $utc != 0 ? ("(UTC" . ($utc >= 0 ? "+" : "") . "$utc)") : "" ?>
+    </h1>
     <ul>
         <?php
         foreach ($holidays as $holiday) {
